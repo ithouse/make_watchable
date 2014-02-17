62aea1971873007896028c320ab280e8c26ee5c5
Dominic Lovell <dom@surfstitch.com>
2014-02-18 08:33:37 +1100

    Fix generators

--- lib/generators/make_watchable/make_watchable_generator.rb
+++ lib/generators/make_watchable/make_watchable_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators/migration'
-require 'rails/generators/active_record'
+require 'rails/generators/migration'
 
-class MakeWatchableGenerator < Rails::Generators::Base
+class MakeWatchableGenerator < ActiveRecord::Generators::Base
   include Rails::Generators::Migration
 
   desc "Generates a migration for the watching model"
@@ -9,10 +9,6 @@ class MakeWatchableGenerator < Rails::Generators::Base
   def self.source_root
     @source_root ||= File.dirname(__FILE__) + '/templates'
   end
-
-  #def self.next_migration_number(path)
-  #  ActiveRecord::Generators::Base.next_migration_number(path)
-  #end
 
   def generate_migration
     migration_template 'migration.rb', 'db/migrate/create_make_watchable_tables'
