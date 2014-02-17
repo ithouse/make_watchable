a700362cf5f9d0e85b3b11292c7abf0a59a7c21b
Dominic Lovell <dom@surfstitch.com>
2014-02-18 07:50:56 +1100

    Add support for Rails 4

--- lib/make_watchable/watching.rb
+++ lib/make_watchable/watching.rb
@@ -2,5 +2,12 @@ module MakeWatchable
   class Watching < ActiveRecord::Base
     belongs_to :watchable, :polymorphic => true
     belongs_to :watcher, :polymorphic => true
+
+    def resource_params
+    	unless params[resource_name].blank?
+      		params.require(resource_name).permit(:watcher, :watchable)
+    	end
+  	end
+  	
   end
 end
