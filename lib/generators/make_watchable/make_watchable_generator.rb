ac98261613b2ec3807b8b80a1ce160b4b3a9273d
Dominic Lovell <dom@surfstitch.com>
2014-02-18 08:45:39 +1100

    Fix generator

--- lib/generators/make_watchable/make_watchable_generator.rb
+++ lib/generators/make_watchable/make_watchable_generator.rb
@@ -1,13 +1,22 @@
 require 'rails/generators/migration'
-require 'rails/generators/migration'
+require 'rails/generators/active_record'
 
-class MakeWatchableGenerator < ActiveRecord::Generators::Base
+class MakeWatchableGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
 
   desc "Generates a migration for the watching model"
 
   def self.source_root
     @source_root ||= File.dirname(__FILE__) + '/templates'
+  end
+
+  def self.next_migration_number(path)
+    unless @prev_migration_nr
+      @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+    else
+      @prev_migration_nr += 1
+    end
+    @prev_migration_nr.to_s
   end
 
   def generate_migration
